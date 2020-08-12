22e46e2afd7f78cc7ee662c60bb2d07dc3f331ce
Nicolas Kiger <nicolas.kiger@gmail.com>
2020-08-12 17:04:27 +0200

    Exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: "french"
+  )
+
+  review = Review.new(
+    content: Faker::Restaurant.review,
+    rating: rand(0..5)
+  )
+
+  review.restaurant = restaurant
+
+  review.save
+end
